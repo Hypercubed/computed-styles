84396b61455d6379c41523c77b0f6a091c83848c
J. Harshbarger <hypercubed@gmail.com>
2015-10-28 14:20:49 +0900

    Fix for IE11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const isDefined = (a) => typeof a !== 'undefined';
+const isUndefined = (a) => typeof a === 'undefined';
 const isObject = (a) => { return a !== null && typeof a === 'object'; };
 
 // from https://github.com/npm-dom/is-dom/blob/master/index.js
@@ -10,6 +11,18 @@ function isNode(val){
 }
 
 const useComputedStyles =  isDefined(window) && isDefined(window.getComputedStyle);
+
+// Gets computed styles for an element
+// from https://github.com/jquery/jquery/blob/master/src/css/var/getStyles.js
+function getComputedStyles(node) {
+  if (useComputedStyles) {
+    var view = node.ownerDocument.defaultView;
+    if ( !view.opener ) view = window;
+    return view.getComputedStyle(node,null);
+  } else {
+    return node.currentStyle || node.style;
+  }
+}
 
 /**
 * Returns a collection of CSS property-value pairs
@@ -26,21 +39,21 @@ function computedStyles(node, target = {}, styleList = true) {
 
   if (styleList === false) return target;
 
-  if (useComputedStyles) {
-    var computed = node.ownerDocument.defaultView.getComputedStyle(node,null);
-    var keysArray = (styleList === true) ? computed : Object.keys(styleList);
+  var computed = getComputedStyles(node);
+
+  if (styleList === true) {
+    var keysArray = useComputedStyles ? computed : Object.keys(computed);
   } else {
-    var computed = isDefined(node.currentStyle) ? node.currentStyle : node.style;
-    var keysArray = (styleList === true) ? Object.keys(computed) : Object.keys(styleList);
+    var keysArray = Object.keys(styleList);
   }
 
   for(let i = 0, l = keysArray.length; i < l; i++){
     let key = keysArray[i];
 
     let def = styleList === true || styleList[key];
-    if (def === false || !isDefined(def)) continue;  // copy never
+    if (def === false || isUndefined(def)) continue;  // copy never
 
-    let value = useComputedStyles ? computed.getPropertyValue(key) : computed[key];
+    let value = /* computed.getPropertyValue(key) || */ computed[key];  // using getPropertyValue causes error in IE11
     if (typeof value !== 'string' || value === '') continue; // invalid value
 
     if (def === true || value !== def ) {  // styleList === true || styleList[key] === true || styleList[key] !== value
